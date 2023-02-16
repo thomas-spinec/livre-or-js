64a6ae754cfe6e7dfe2045435a398ad6e0898486
thomas-spinec <thomas.spinec@laplateforme.io>
2023-02-16 16:36:16 +0100

    livre d'or fonctionnel

---
+++ class/Comment.php
@@ -0,0 +1,81 @@
+<?php
+// création de la class
+
+class Comment
+{
+    // attributs
+    private $bdd;
+
+    // constructeur
+    public function __construct()
+    {
+        // connection à la BDD avec PDO
+        // en local ////////////////////
+        $servername = 'localhost';
+        $dbname = 'livreor';
+        $db_username = 'root';
+        $db_password = '';
+
+        // en ligne ///////////////////
+        // $servername = 'localhost';
+        // $dbname = 'thomas-spinec_livreor';
+        // $db_username = 'adminbdd';
+        // $db_password = 'basededonnees';
+
+
+        // essaie de connexion
+        try {
+            $this->bdd = new PDO("mysql:host=$servername;dbname=$dbname; charset=utf8", $db_username, $db_password);
+
+            // On définit le mode d'erreur de PDO sur Exception
+            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //echo "Connexion réussie"; 
+            $this->bdd->exec("set names utf8");
+        }
+        // si erreur, on capture les exceptions, s'il y en a une on affiche les infos
+        catch (PDOException $e) {
+            echo "Echec de la connexion : " . $e->getMessage();
+            exit;
+        }
+    }
+
+    // méthodes
+    // ajout de commentaire
+    public function addComment($commentaire, $idUser)
+    {
+        // requête pour ajouter un commentaire
+        $requete = "INSERT INTO commentaires (commentaire, date, id_utilisateur) VALUES (:commentaire, NOW(), :idUser)";
+
+        // préparation de la requête
+        $insert = $this->bdd->prepare($requete);
+
+        // exécution de la requête avec liaison des paramètres
+        $insert->execute(array(
+            'commentaire' => $commentaire,
+            'idUser' => $idUser
+        ));
+    }
+
+    // affichage des commentaires
+    public function displayComment()
+    {
+        // requête pour récupérer tout ce qu'il y a dans la base de données commentaires, ainsi que le login dans la base de donnée utilisateurs correspondant à l'id_utilisateurs de la base de données commentaires
+        $requete = "SELECT commentaires.id, commentaires.commentaire, DATE_FORMAT(commentaires.date, '%d/%m/%Y') as date, utilisateurs.login FROM commentaires INNER JOIN utilisateurs ON commentaires.id_utilisateur = utilisateurs.id ORDER BY id DESC";
+
+        // exécution de la requête
+        $select = $this->bdd->query($requete);
+
+        // récupération dans un fetch ASSOC
+        // $result = $select->fetch(PDO::FETCH_ASSOC);
+        // affichage des commentaires
+        while ($reponse = $select->fetch(PDO::FETCH_ASSOC)) {
+?>
+            <tr>
+                <td class='date'><?= $reponse['date'] ?></td>
+                <td class='user'><?= $reponse['login'] ?></td>
+                <td class='comm'><?= $reponse['commentaire'] ?></td>
+            </tr>
+<?php
+        }
+    }
+}
---
+++ livre-or.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Livre d'or</title>
+    <link rel="stylesheet" href="site.css">
+    <link rel="icon" type="images/png" sizes="64x64" href="img/Logo_onglet.png" />
+</head>
+
+<body>
+    <!-- Include -->
+    <?php
+    require_once 'class/Comment.php';
+    $comment = new Comment();
+    ?>
+    <!-- /Include -->
+
+    <!-- Header -->
+    <?php
+    session_start();
+    require_once 'includes/header.php';
+    ?>
+    <!-- /Header -->
+
+    <!-- Main -->
+    <main>
+        <section id="livre-or">
+            <h1>Livre d'or</h1>
+            <table class="commentaires">
+                <thead>
+                    <tr>
+                        <th class="date">Posté le</th>
+                        <th class="user">Par l'utilisateur</th>
+                        <th class="comm">Commentaires</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    $comments = $comment->displayComment();
+                    ?>
+                </tbody>
+            </table>
+        </section>
+        <section id="leaveComment">
+            <?php
+            if ($user->isConnected()) {
+            ?>
+                <br>
+                <div class="center">
+                    <a href='commentaire.php'><button>Laisser un commentaire</button></a>
+                </div>
+            <?php
+            } else {
+            ?>
+                <div class="center">
+                    <p>Vous devez être connecté pour laisser un commentaire</p>
+                    <a href='user.php?sign=conn'><button>Se connecter</button></a>
+                </div>
+            <?php
+            }
+            ?>
+        </section>
+    </main>
+    <!-- /Main -->
+
+    <!-- Footer -->
+    <?php require_once 'includes/footer.php'; ?>
+    <!-- /Footer -->
+
+
+</body>
